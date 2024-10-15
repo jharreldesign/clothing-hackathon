9f5aaff1ffc1f7089c7548c21354ff68c16d61ba
mparente88 <parente.molly@gmail.com>
2024-10-15 14:24:45 -0700

    testing

--- Models/cart.js
+++ Models/cart.js
@@ -1,18 +1,22 @@
-const { Schema } = require ('mongoose')
+const { Schema } = require("mongoose");
 
-const Cart = new Schema (
-    {
-        userId: { type: String, required: true },
-        items: [
-            { 
-                productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-                quantity: { type: Number, required: true },
-                price: { type: Number, required: true }
-            }
-        ],
-        totalPrice: { type: Number, required: true },
-    },
-        { timestamps: true }
-)
+const Cart = new Schema(
+  {
+    userId: { type: String, required: true },
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = Cart
+module.exports = Cart;
