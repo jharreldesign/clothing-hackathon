05f41bbfea1a20794e166ae394fb5c864a9ddf93
lanewells <wellsdelaneyk@gmail.com>
2024-10-16 22:27:21 -0700

    Stepped on everyone's toes and hopefully didn't break anything. Also created function to populate front end product cards using db

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # clothing-hackathon
+
+## Group Members
+Jason Harrel
+Molly Parente
+Lane Wells
+
+### Citations
+ - [mongoose.js.com](https://mongoosejs.com/docs/validation.html#update-validators)
+    - *Mongoose .runValidators* used for updating cart and updating product
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require('cors')
 const PORT = process.env.PORT || 3001
 const db = require("./db")
 const bodyParser = require("body-parser")
@@ -11,6 +12,7 @@ const userController = require("./controllers/userController")
 app.use(express.json())
 app.use(logger("dev"))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get("/", async (req, res) => {
   res.send("Our pants!! They're on fire!!")
@@ -34,6 +36,7 @@ app.get("/products/chinos", productController.getChinos)
 app.get("/products/palazzopants", productController.getPalazzoPants)
 app.get("/products/linenpants", productController.getLinenPants)
 app.get("/products/onSale", productController.getProductsOnSale)
+app.get('/products/lowstock', productController.getProductsLowStock)
 
 app.get(
   "/products/price/ascending",
@@ -48,20 +51,31 @@ app.get("/products/size/s", productController.getProductsS)
 app.get("/products/size/m", productController.getProductsM)
 app.get("/products/size/l", productController.getProductsL)
 app.get("/products/size/xl", productController.getProductsXL)
+app.get('/products/color/blues', productController.getProductsBlues)
+app.get('/products/color/blacks', productController.getProductsBlacks)
+app.get('/products/color/greens', productController.getProductsGreens)
+app.get('/products/color/neutrals', productController.getProductsNeutrals)
+app.get('/products/color/reds', productController.getProductsReds)
+app.get('/products/color/multicolored', productController.getProductsMulticolored)
+app.get('/products/color/grayscale', productController.getProductsGrayscale)
 
 // show routes
 app.get("/products/:id", productController.getProductById)
 app.get("/users/:id", userController.getUserById)
 
 app.get("/products/brands/:brand", productController.getProductsByBrand)
-app.get("/users/name/:name", userController.getUserByName)
 app.get("/products/price/:max", productController.getProductsMaxPrice)
+app.get("/users/name/:name", userController.getUserByName)
 
 // crud routes
 app.post("/product", productController.createProduct)
-app.put("/product/:id", productController.updateProduct)
-app.delete("/product/:id", productController.deleteProduct)
+app.post("/cart", cartController.createCart)
 app.post("/users", userController.createUser)
+app.put("/product/:id", productController.updateProduct)
+app.put("/cart/:id", cartController.updateCart)
+//app.put('/cart/additem/:id', cartController.addItemToCart)
+app.delete("/product/:id", productController.deleteProduct)
+app.delete("/cart/:id", cartController.deleteCart)
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
