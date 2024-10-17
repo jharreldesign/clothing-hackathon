526c6c725afcd24bdccc012bba7acea7f24c7fea
mparente88 <parente.molly@gmail.com>
2024-10-17 14:39:56 -0700

    testing

--- client/indexScript.js
+++ client/indexScript.js
@@ -1,22 +1,22 @@
 let currentIndex = 0
-const totalSlides = 12 // Total number of slides
-const itemsToShow = 4 // Number of items to show at a time
+const totalSlides = 12
+const itemsToShow = 4
 
 const updateCarousel = () => {
-  const offset = (currentIndex * -100) / itemsToShow // Move left based on the index
+  const offset = (currentIndex * -100) / itemsToShow
   document.querySelector(
     ".carousel-container"
   ).style.transform = `translateX(${offset}%)`
 }
 
 document.querySelector(".next").addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % (totalSlides - itemsToShow + 1) // Loop back to start
+  currentIndex = (currentIndex + 1) % (totalSlides - itemsToShow + 1)
   updateCarousel()
 })
 
 document.querySelector(".prev").addEventListener("click", () => {
   currentIndex =
     (currentIndex - 1 + (totalSlides - itemsToShow + 1)) %
-    (totalSlides - itemsToShow + 1) // Loop back to end
+    (totalSlides - itemsToShow + 1)
   updateCarousel()
 })
